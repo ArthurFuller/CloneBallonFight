d92bc8cca50d86d6a44872c1125ff509816505b8
ArthurFuller <arthurlgfuller@gmail.com>
2026-03-17 19:34:49 -0300

    FINALIZAÇÃO DA BASE DO JOGO
    Implementação da movimentação infinita no eixo X do Player.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float vel = 5.0f;
     [SerializeField] float upForce = 5.0f;
     //[SerializeField] float gravity = 1.5f;
-    [SerializeField] float limiteX = 4.0f;
+    [SerializeField] float limiteX = 7.5f;
 
     [SerializeField] GameController gameController;
 
@@ -37,13 +37,18 @@ public class PlayerController : MonoBehaviour
         float x = Input.GetAxis("Horizontal") * vel * Time.deltaTime;
         transform.Translate(x, 0, 0);
 
-        if (Input.GetAxis("Horizontal") > 0) 
+        if (Input.GetAxis("Horizontal") < 0) 
         { 
             sr.flipX = true;
         }
         else
         {
             sr.flipX = false;
+        }
+
+        if (transform.position.x < -limiteX || transform.position.x > limiteX)
+        {
+            transform.position = new Vector2(transform.position.x * -1, transform.position.y);
         }
 
         
